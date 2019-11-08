13f0ed5851555a691bd389fadd52738b8f6414e3
Larry1149 <chunvoon1149@gmail.com>
2019-11-08 22:35:26 +0800

    test

--- app/src/main/java/com/example/livechatv2/RegisterActivity.java
+++ app/src/main/java/com/example/livechatv2/RegisterActivity.java
@@ -12,8 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.example.livechatv2.MainActivity;
-import com.example.livechatv2.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
